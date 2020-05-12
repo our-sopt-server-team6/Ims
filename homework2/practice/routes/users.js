f8469856588c5cc74230d08c6f70874dc64f6ee9
gudwnsrh@gmail.com <gudwnsrh@gmail.com>
2020-05-12 17:14:32 +0900

    homework3

--- homework2/practice/routes/users.js
+++ homework2/practice/routes/users.js
@@ -5,37 +5,6 @@ let util = require('../modules/util');
 let statusCode = require('../modules/statusCode');
 let resMessage = require('../modules/responseMessage');
 
-/* 
-    ✔️ sign up
-    METHOD : POST
-    URI : localhost:3000/user/signup
-    REQUEST BODY : id, name, password, email
-    RESPONSE STATUS : 200 (OK)
-    RESPONSE DATA : User ID
-*/
-
-// router.post('/signup', async (req, res) => {
-//     const { id, name, password, email } = req.body;
-//     User.push({id, name, password, email});
-//     res.status(200).send(User);
-// });
-
-// 2단계
-// router.post('/signup', async (req, res) => {
-//     const { id, name, password, email } = req.body;
-//     // request data 확인 - 없다면 Bad Request 반환
-//     if ( !id || !name || !password || !email ) {
-//         return res.status(400).send({ message: 'BAD REQUEST' });
-//     }
-//     //already ID
-//     if (User.filter(user => user.id == id).length > 0) {
-//         return res.status(400).send({ message: 'ALREADY ID' });
-//     }
-//     User.push({id, name, password, email});
-//     res.status(200).send(User);
-// });
-
-// 3단계
 router.post('/signup', async (req, res) => {
     const {
         id,
@@ -43,13 +12,13 @@ router.post('/signup', async (req, res) => {
         password,
         email
     } = req.body;
-    // request data 확인 - 없다면 Null Value 반환
+
     if (!id || !name || !password || !email) {
         res.status(statusCode.BAD_REQUEST)
             .send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
         return;
     }
-    //already ID
+
     if (UserModel.filter(user => user.id == id).length > 0) {
         res.status(statusCode.BAD_REQUEST)
             .send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
@@ -76,6 +45,30 @@ router.post('/signup', async (req, res) => {
     RESPONSE DATA : User ID
 */
 router.post('/signin', async (req, res) => {
+ const{
+    id,
+    password,
+ } = req.body;
+
+ if(!id||password){
+   res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
+   return ;
+ }
+
+  const user = User.filter(user=>user.id==id);
+
+  if(user.length==0){
+    res.status(statusCode.BAD_REQUEST)
+    .send(util.fail(statusCode.BAD_REQUEST,resMessage.NO_USER));
+    return ;
+
+  }
+  
+  if(user[0].password !== password){
+    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.MISS_MATCH_PW));
+    return ;
+  }
+  res.status(statusCode.OK).send(util.success(statusCode.OK,resMessage.LOGIN_SUCCESS,{userId:id}))
     // request body 에서 데이터 가져오기
     // request data 확인 - 없다면 Null Value 반환
     // 존재하는 아이디인지 확인 - 없다면 No user 반환
